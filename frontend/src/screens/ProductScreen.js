767fc04a8388a323896e2b6c6680a96ecbe9179a
Laurie Waller <lauriewaller7@gmail.com>
2021-08-31 22:24:15 -0700

    fix div location

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -18,19 +18,20 @@ export default function ProductScreen(props) {
         <div className="col-2">
           <img className="large" src={product.image} alt={product.name}></img>
         </div>
-        <div className="col-1"></div>
-        <ul>
-          <li>
-            <h1>{product.name}</h1>
-          </li>
-          <li>
-            <Rating rating={product.rating} numReviews={product.numReviews} />
-          </li>
-          <li>Price: ${product.price}</li>
-          <li>
-            Description:<p>{product.description}</p>
-          </li>
-        </ul>
+        <div className="col-1">
+          <ul>
+            <li>
+              <h1>{product.name}</h1>
+            </li>
+            <li>
+              <Rating rating={product.rating} numReviews={product.numReviews} />
+            </li>
+            <li>Price: ${product.price}</li>
+            <li>
+              Description:<p>{product.description}</p>
+            </li>
+          </ul>
+        </div>
         <div className="col-1">
           <div className="card card-body">
             <ul>
