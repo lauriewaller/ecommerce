152b1bb2a245b683b83d9f63af3e5cdfa88621c2
Laurie Waller <lauriewaller7@gmail.com>
2021-09-06 12:53:09 -0700

    add axios updates to project

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,21 @@
-import React from "react";
-import data from "./../data";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Product from "./../components/Product";
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]); // sets default value to empty array
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products"); // the array in backend will be transfreerd to data in frontend
+      setProducts(data);
+    };
+    fetchData();
+  }, []); // fills the products. another hook that happens when comp did mounts to page. first param is a function and second is an array that accepts dependencies. This has no dependencies. this function will run only one time
+  //sends ajax req to backaend and fetch products
   return (
     <div>
       <div className="row center">
-        {data.products.map((product) => (
+        {products.map((product) => (
           // key here allows each card to be separated based on id from each obj
           <Product key={product.id} product={product} />
         ))}
