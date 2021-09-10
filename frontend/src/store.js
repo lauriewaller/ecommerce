0e0ea11509ef9ea7645cda1396093fec394b1229
Laurie Waller <lauriewaller7@gmail.com>
2021-09-09 21:52:15 -0700

    add reduc to ProductScreen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,9 @@
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import { productListReducer } from "./reducers/productReducers";
+import {
+  productDetailsReducer,
+  productListReducer,
+} from "./reducers/productReducers";
 
 const initialState = {};
 // const reducer = (state, action) => {
@@ -9,6 +12,7 @@ const initialState = {};
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
