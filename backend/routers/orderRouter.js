77397d7edea318490eb3d2f46f039855a1d08086
Laurie Waller <lauriewaller7@gmail.com>
2021-09-18 13:03:51 -0700

    add /:id path to userRouter

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -80,6 +80,18 @@ orderRouter.put(
   })
 );
 
+userRouter.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ message: "User Not Found" });
+    }
+  })
+);
+
 export default orderRouter;
 
 //expressAsyncHandler allows us to catch errors with async functions
