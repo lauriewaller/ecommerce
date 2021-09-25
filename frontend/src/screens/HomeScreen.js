75f338a3b86f03ac6413157996a6928200988da6
Laurie Waller <lauriewaller7@gmail.com>
2021-09-24 20:20:57 -0700

    create working category call in HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,15 +3,15 @@ import Product from "./../components/Product";
 import MessageBox from "../components/MessageBox";
 import LoadingBox from "../components/LoadingBox";
 import { useDispatch, useSelector } from "react-redux";
-import { listProducts } from "../actions/productActions";
+import { listCategories } from "../actions/productActions";
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+  const categoryList = useSelector((state) => state.categoryList);
+  const { loading, error, categories } = categoryList;
 
   useEffect(() => {
-    dispatch(listProducts());
+    dispatch(listCategories());
   }, [dispatch]); // fills the products. another hook that happens when comp did mounts to page. first param is a function and second is an array that accepts dependencies. This has no dependencies. this function will run only one time
   //sends ajax req to backend and fetch products
 
@@ -23,9 +23,9 @@ export default function HomeScreen() {
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
         <div className="row center">
-          {products.map((product) => (
+          {categories.map((product) => (
             // key here allows each card to be separated based on id from each obj
-            <Product key={product._id} product={product} />
+            <p>category</p>
           ))}
         </div>
       )}
