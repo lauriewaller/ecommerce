3e452c20598c30f45cdb8a8c79796322a74e31d4
Laurie Waller <lauriewaller7@gmail.com>
2021-09-24 23:40:44 -0700

    move header code to separate Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Link, Route } from "react-router-dom";
-import { signout } from "./actions/userActions";
 import PrivateRoute from "./components/PrivateRoute";
-
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import OrderHistoryScreen from "./screens/OrderHistoryScreen";
@@ -16,63 +13,16 @@ import SigninScreen from "./screens/SigninScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
+import Header from "./components/Header";
 
 function App() {
-  const cart = useSelector((state) => state.cart);
-  const { cartItems } = cart;
-  const userSignin = useSelector((state) => state.userSignin);
-  const { userInfo } = userSignin;
-  const dispatch = useDispatch();
-  const signoutHandler = () => {
-    dispatch(signout());
-  };
-
   return (
     <BrowserRouter>
       <div className="grid-container">
-        <header className="row">
-          <div>
-            <Link className="brand" to="/">
-              amazona
-            </Link>
-          </div>
-          <div>
-            <Link to="/cart">
-              Cart
-              {cartItems.length > 0 && (
-                <span className="badge">{cartItems.length}</span>
-              )}
-            </Link>
-            {userInfo ? (
-              <div className="dropdown">
-                <Link to="#">
-                  {userInfo.name} <i className="fa fa-caret-down"></i>{" "}
-                </Link>
-                <ul className="dropdown-content">
-                  <li>
-                    <Link to="/profile">User Profile</Link>
-                  </li>
-                  <li>
-                    <Link to="/orderhistory">Order History</Link>
-                  </li>
-                  <li>
-                    <Link to="#signout" onClick={signoutHandler}>
-                      Sign Out
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <Link to="/signin">Sign In</Link>
-            )}
-          </div>
-        </header>
+        <Header />
         <main>
-          {/* id is optional in CartScreen component, hence the question mark */}
-
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
-
           <Route path="/category/:category" component={CategoryScreen}></Route>
           <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/signin" component={SigninScreen}></Route>
@@ -94,3 +44,7 @@ function App() {
 }
 
 export default App;
+
+{
+  /* id is optional in CartScreen component, hence the question mark */
+}
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { signout } from "./../actions/userActions";
+
+export default function Header() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
+
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <Link className="brand" to="/">
+            Laurie
+          </Link>
+        </div>
+        <div>
+          <Link to="/cart">
+            Cart
+            {cartItems.length > 0 && (
+              <span className="badge">{cartItems.length}</span>
+            )}
+          </Link>
+          {userInfo ? (
+            <div className="dropdown">
+              <Link to="#">
+                {userInfo.name} <i className="fa fa-caret-down"></i>{" "}
+              </Link>
+              <ul className="dropdown-content">
+                <li>
+                  <Link to="/profile">User Profile</Link>
+                </li>
+                <li>
+                  <Link to="/orderhistory">Order History</Link>
+                </li>
+                <li>
+                  <Link to="#signout" onClick={signoutHandler}>
+                    Sign Out
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <Link to="/signin">Sign In</Link>
+          )}
+        </div>
+      </header>
+    </div>
+  );
+}
