07b4fa4a0c546f9de91586e384cc181389c153be
Laurie Waller <lauriewaller7@gmail.com>
2021-10-03 16:04:25 -0700

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,15 @@ app.get("/api/config/paypal", (req, res) => {
   //sb stands for sandbox
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+// the code below makes it possible to serve react files
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// }); removed this bc root address should point to build version of react
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
